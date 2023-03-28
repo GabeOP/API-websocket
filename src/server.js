d84b4109c829f84e251ae41900266763b6f31fcd
GabeOP <gabrieloliveirap1@hotmail.com>
2023-03-28 13:32:29 -0300

    feat: criacao de usuario após envio do form

--- src/server.js
+++ src/server.js
@@ -3,36 +3,42 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer,{cors: {origin: "*"}});
+const io = new Server(httpServer, { cors: { origin: "*" } });
 
+//body-parser para analisar o corpo da requisição
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const User = require("./model/User")
 // Rota HTTP
-// app.get('/', (req, res) => {
-//   res.send('Bem-vindo à minha API!');
-// });
-
-
-// Evento de conexão na rota ADMIN
-const admin = io.of("/admin");
-admin.on("connection", socket =>{
-  console.log("algm conectou na rota ADMIN " + socket.id)
-})
-
-// Evento de conexão na rota INICIO
-const inicio = io.of("/inicio")
-inicio.on('connection', (socket) => {
-  console.log('Um usuário conectou na rota INICIO' + socket.id );
+app.post('/cadastro', async(req, res) => {
+  const {name, password} = req.body;
+  const user = new User ({name: name, password:password});
+  
+  try{
+    await user.save();
+  }catch(error){
+    console.log(error)
+  }
+  res.redirect("http://127.0.0.1:5500/sala1/sala1.html")
 });
+
 
 // Evento de conexão na rota SALA 1
 const sala1 = io.of("/sala1");
-sala1.on("connect", (socket)=>{
-  console.log("um usuário conectou-se à sala 1")
-  socket.on('chat message', (msg)=>{
-    console.log(`${socket.id}: ${msg}`);
-    sala1.emit('chat message', msg);
-  })
-})
+sala1.on("connect", (socket) => {
+  console.log("Um usuário conectou-se à sala 1");
+  socket.on("chat message", (msg) => {
+    console.log(`${msg}`);
+    sala1.emit("chat message", msg);
+    
+  });
+
+});
+
+const connectDb = require("./data/db");
+connectDb();
 
 httpServer.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("Rodando na porta 3000");
 });
